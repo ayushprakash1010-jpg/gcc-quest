808ceb692918b25800a003228dbe3c601a673cf8
ayushprakash1010-jpg <ayushprakash1010@gmail.com>
2026-08-06 01:38:20 +0530

    revert to personal profile posting

--- apps/web/auth.ts
+++ apps/web/auth.ts
@@ -48,7 +48,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       clientSecret: process.env.AUTH_LINKEDIN_SECRET,
       authorization: {
         params: {
-          scope: "openid profile email w_member_social w_organization_social",
+          scope: "openid profile email w_member_social",
         },
       },
     }),
